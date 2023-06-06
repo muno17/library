335455e02679e25943c26bad73105e64777901b1
muno17 <munozd17@gmail.com>
2023-06-06 17:39:07 -0400

    add skeleton

---
+++ script.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return title + ", " + pages + "pages, " + read;
+    }
+}
