21b8299eb6efb54828d6e68528feb7164fbb8567
muno17 <munozd17@gmail.com>
2023-06-07 23:27:02 -0400

    add form reset line to bookButton, add validation checking in addBookToLibrary

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let myLibrary = [
-    {
-        title: "Rafa and the Rafettes",
-        author: "Rafaela",
-        pages: 420,
-        read: true,
-    },
-    {
-        title: "Rafa and the Rafettes 2",
-        author: "Rafaela",
-        pages: 69,
-        read: false,
-    },
+    // {
+    //     title: "Rafa and the Rafettes",
+    //     author: "Rafaela",
+    //     pages: 420,
+    //     read: true,
+    // },
+    // {
+    //     title: "Rafa and the Rafettes 2",
+    //     author: "Rafaela",
+    //     pages: 69,
+    //     read: false,
+    // },
 ];
 
 function Book(title, author, pages, read) {
@@ -25,7 +25,14 @@ function Book(title, author, pages, read) {
 }
 
 let bookButton = document.getElementById('bookButton');
-bookButton.addEventListener('click', () => {addBookToLibrary()});
+bookButton.addEventListener('click', (event) => {
+    // event.preventDefault();
+    addBookToLibrary();
+    bookDisplayer(myLibrary);
+    document.getElementById('newBookForm').reset();
+});
+
+
 
 
 function bookDisplayer(arr) {
@@ -35,11 +42,13 @@ function bookDisplayer(arr) {
     grid.innerHTML = "";
 
     // loop through the array and add each book to the grid
-    for (let i = 0; i < length - 1; i++) {
+    for (let i = 0; i < length; i++) {
         let newDiv = document.createElement('div');    
         let linebreak = document.createElement('br');    
 
         let current = arr[i];
+        if (current === undefined) continue;
+
         newDiv.innerHTML += `Title: ${current.title}`;
         newDiv.appendChild(linebreak);
         newDiv.innerHTML += `Author: ${current.author}`;
@@ -53,21 +62,33 @@ function bookDisplayer(arr) {
     }
 }
 
+// remove form submit default setting
+let form = document.getElementById('newBookForm');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
 // take form fields and create new book when add book is pressed
 function addBookToLibrary(book) {
-    let newTitle = document.getElementById("titleid");
-    let newAuthor = document.getElementById("authorid");
-    let newPages = document.getElementById("pagesid");
 
-    // create new book taking values of form fields
-    let newBook = new Book;
-    newBook.title = newTitle.innerHTML;
-    newBook.author = newAuthor.innerHTML;
-    newBook.pages = newPages.innerHTML;
-    newBook.read = true;
+    let newTitle = document.getElementById("titleid").value;
+    let newAuthor = document.getElementById("authorid").value;
+    let newPages = document.getElementById("pagesid").value;
+    let switchInfo= document.getElementById("readSwitch");
 
-    myLibrary.push(book);
-    bookDisplayer(myLibrary);
+    // add book if all fields are valid
+    if (newTitle && newAuthor && newPages){
+        // create new book taking values of form fields
+        let newBook = new Book;
+        newBook.title = newTitle;
+        newBook.author = newAuthor;
+        newBook.pages = newPages;
+        newBook.read = true;
+
+        if (newBook.title != undefined) {
+            myLibrary.push(newBook);
+        }
+    }
     // run bookDisplayer after book has been added
 }
 
